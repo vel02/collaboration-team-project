d7faca8d17073e84c62f0134f55df47c8afb1fec
Ariel Namias Austria <arielaustria021@gmail.com>
2020-12-10 09:05:14 +0800

    version 1: register email with verification.

--- YesCredit/app/src/main/java/cs/collaboration/yescredit/ui/signup/SignUpActivity.java
+++ YesCredit/app/src/main/java/cs/collaboration/yescredit/ui/signup/SignUpActivity.java
@@ -1,16 +1,29 @@
 package cs.collaboration.yescredit.ui.signup;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
 
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import javax.inject.Inject;
 
 import cs.collaboration.yescredit.databinding.ActivitySignUpBinding;
+import cs.collaboration.yescredit.ui.login.LoginActivity;
 import cs.collaboration.yescredit.viewmodel.ViewModelProviderFactory;
 import dagger.android.support.DaggerAppCompatActivity;
 
+import static android.text.TextUtils.isEmpty;
+
 public class SignUpActivity extends DaggerAppCompatActivity {
+
+    private static final String TAG = "SignUpActivity";
+
+    private static final String DOMAIN_NAME = "gmail.com";
 
     @Inject
     ViewModelProviderFactory providerFactory;
@@ -24,6 +37,102 @@ public class SignUpActivity extends DaggerAppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         viewModel = new ViewModelProvider(this, providerFactory).get(SignUpViewModel.class);
+        register();
+        subscribeObservers();
+    }
+
+    private void register() {
+        binding.contentSignup.signupContentRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: attempting to register.");
+
+                if (!isEmpty(binding.contentSignup.signupContentEmail.getText().toString())
+                        && !isEmpty(binding.contentSignup.signupContentPassword.getText().toString())
+                        && !isEmpty(binding.contentSignup.signupContentConfirmPassword.getText().toString())) {
+
+                    if (isValidDomain(binding.contentSignup.signupContentEmail.getText().toString())) {
+
+                        if (isPasswordMatch(binding.contentSignup.signupContentPassword.getText().toString(),
+                                binding.contentSignup.signupContentConfirmPassword.getText().toString())) {
+
+                            viewModel.registerNewEmail(binding.contentSignup.signupContentEmail.getText().toString(),
+                                    binding.contentSignup.signupContentPassword.getText().toString());
+
+                        } else {
+                            Toast.makeText(SignUpActivity.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                        }
+
+                    } else {
+                        Toast.makeText(SignUpActivity.this, "Please Register with valid Gmail Account", Toast.LENGTH_SHORT).show();
+                    }
+
+                } else {
+                    Toast.makeText(SignUpActivity.this, "You must fill out all the fields", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        hideSoftKeyboard();
+    }
+
+
+    private void subscribeObservers() {
+        viewModel.observeProgressBarState().observe(this, new Observer<SignUpViewModel.State>() {
+            @Override
+            public void onChanged(SignUpViewModel.State state) {
+                if (state != null) {
+                    switch (state) {
+                        case VISIBLE:
+                            showDialog();
+                            break;
+                        case INVISIBLE:
+                            hideDialog();
+                            break;
+                    }
+                }
+            }
+        });
+
+        viewModel.observeDirectToLoginScreen().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean aBoolean) {
+                if (aBoolean != null) {
+                    if (aBoolean) {
+                        Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                        finish();
+                    }
+                }
+            }
+        });
 
     }
+
+    private boolean isValidDomain(String email) {
+        Log.d(TAG, "isValidDomain: isValidDomain: verifying email has correct domain: " + email);
+        String domain = email.substring(email.indexOf("@") + 1).toLowerCase();
+        Log.d(TAG, "isValidDomain: users domain: " + domain);
+        return domain.equals(DOMAIN_NAME);
+    }
+
+    private boolean isPasswordMatch(String password, String confirm_password) {
+        return password.equals(confirm_password);
+    }
+
+    private void showDialog() {
+        binding.contentSignup.progressBar.setVisibility(View.VISIBLE);
+    }
+
+    private void hideDialog() {
+        if (binding.contentSignup.progressBar.getVisibility() == View.VISIBLE)
+            binding.contentSignup.progressBar.setVisibility(View.INVISIBLE);
+    }
+
+    private void hideSoftKeyboard() {
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+    }
+
 }
--- YesCredit/app/src/main/java/cs/collaboration/yescredit/ui/signup/SignUpViewModel.java
+++ YesCredit/app/src/main/java/cs/collaboration/yescredit/ui/signup/SignUpViewModel.java
@@ -2,7 +2,16 @@ package cs.collaboration.yescredit.ui.signup;
 
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import javax.inject.Inject;
 
@@ -10,8 +19,72 @@ public class SignUpViewModel extends ViewModel {
 
     private static final String TAG = "SignUpViewModel";
 
+    private final MutableLiveData<State> progressBarState = new MutableLiveData<>();
+    private final MutableLiveData<Boolean> redirectToLoginScreen = new MutableLiveData<>();
+    private final FirebaseAuth auth;
+
+
     @Inject
     public SignUpViewModel() {
         Log.d(TAG, "SignUpViewModel: view model is working...");
+        auth = FirebaseAuth.getInstance();
+        redirectToLoginScreen.setValue(false);
     }
+
+    /*
+        Resource Message
+        - Unable to Register
+        - Send Verification Email
+        - Couldn't Send Verification Email
+     */
+
+    public void registerNewEmail(String email, String password) {
+        progressBarState.setValue(State.VISIBLE);
+
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "onComplete: " + task.isSuccessful());
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "onComplete: uid: " + auth.getCurrentUser().getUid());
+
+                            sendVerificationEmail();
+                            auth.signOut();
+                            redirectToLoginScreen.setValue(true);
+
+                        } else {
+                            Log.d(TAG, "onComplete: Unable to Register");
+                        }
+                        progressBarState.setValue(State.INVISIBLE);
+                    }
+                });
+    }
+
+    public void sendVerificationEmail() {
+        FirebaseUser user = auth.getCurrentUser();
+        if (user != null) {
+            user.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()) {
+                        Log.d(TAG, "onComplete: Send Verification Email");
+                    } else {
+                        Log.d(TAG, "onComplete: Couldn't Send Verification Email");
+                    }
+                }
+            });
+        }
+    }
+
+    public LiveData<State> observeProgressBarState() {
+        return progressBarState;
+    }
+
+    public LiveData<Boolean> observeDirectToLoginScreen() {
+        return redirectToLoginScreen;
+    }
+
+    public enum State {VISIBLE, INVISIBLE}
+
 }
