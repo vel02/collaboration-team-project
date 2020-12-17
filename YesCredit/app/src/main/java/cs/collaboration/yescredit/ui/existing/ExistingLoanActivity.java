6a9b0107146313e3caa3d01124801c63ae99b68e
Ariel Namias Austria <arielaustria021@gmail.com>
2020-12-17 12:08:09 +0800

    version 1: existing Loan activity implementation.

--- YesCredit/app/src/main/java/cs/collaboration/yescredit/ui/existing/ExistingLoanActivity.java
+++ YesCredit/app/src/main/java/cs/collaboration/yescredit/ui/existing/ExistingLoanActivity.java
@@ -1,16 +1,27 @@
 package cs.collaboration.yescredit.ui.existing;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import javax.inject.Inject;
 
 import cs.collaboration.yescredit.BaseActivity;
 import cs.collaboration.yescredit.databinding.ActivityExistingLoanBinding;
+import cs.collaboration.yescredit.ui.allowable.AllowableActivity;
+import cs.collaboration.yescredit.ui.apply.ApplyActivity;
 import cs.collaboration.yescredit.viewmodel.ViewModelProviderFactory;
 
+import static cs.collaboration.yescredit.util.Utility.currencyFormatter;
+
 public class ExistingLoanActivity extends BaseActivity {
+
+    private static final String TAG = "ExistingLoanActivity";
 
     @Inject
     ViewModelProviderFactory providerFactory;
@@ -24,8 +35,65 @@ public class ExistingLoanActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         viewModel = new ViewModelProvider(this, providerFactory).get(ExistingLoanViewModel.class);
+        navigation();
+        subscribeObservers();
+        viewModel.getOnGoingLoan();
 
     }
+
+    private void subscribeObservers() {
+        viewModel.observerLoan().observe(this, loan -> {
+            if (loan != null) {
+                Log.d(TAG, "onChanged: " + loan);
+                binding.contentExistingLoan.contentExistingExistingRoot.setVisibility(View.VISIBLE);
+                binding.contentExistingLoan.contentExistingApplyRoot.setVisibility(View.GONE);
+
+                String repayment = "PHP " + currencyFormatter(loan.getRepayment_loan());
+                String schedule = loan.getRepayment_days() + " Days to Pay";
+                String interest = "PHP " + currencyFormatter(loan.getRepayment_interest());
+                String total = "PHP " + currencyFormatter(loan.getRepayment_total());
+
+                binding.contentExistingLoan.contentExistingRepayment.setText(repayment);
+                binding.contentExistingLoan.contentExistingDaysToPay.setText(schedule);
+                binding.contentExistingLoan.contentExistingDate.setText(loan.getRepayment_date());
+                binding.contentExistingLoan.contentExistingLoan.setText(repayment);
+                binding.contentExistingLoan.contentExistingInterest.setText(interest);
+                binding.contentExistingLoan.contentExistingTotal.setText(total);
+            } else {
+                binding.contentExistingLoan.contentExistingExistingRoot.setVisibility(View.GONE);
+                binding.contentExistingLoan.contentExistingApplyRoot.setVisibility(View.VISIBLE);
+                String repayment = "PHP 0.00";
+                binding.contentExistingLoan.contentExistingRepayment.setText(repayment);
+            }
+        });
+
+        viewModel.observeIsAllowed().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean isNotAllowed) {
+                if (isNotAllowed) {
+                    Toast.makeText(ExistingLoanActivity.this, "You still have On-Going Transaction", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(ExistingLoanActivity.this, "You are allowed to make new Transaction", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(ExistingLoanActivity.this, ApplyActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
+    }
+
+    private void navigation() {
+        binding.contentExistingLoan.contentExistingAllowable.setOnClickListener(v -> {
+            Intent intent = new Intent(ExistingLoanActivity.this, AllowableActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
+        binding.contentExistingLoan.contentExistingApply.setOnClickListener(v -> {
+            viewModel.checkPendingLoan();
+        });
+    }
+
 
     @Override
     protected void onResume() {
--- YesCredit/app/src/main/java/cs/collaboration/yescredit/ui/existing/ExistingLoanViewModel.java
+++ YesCredit/app/src/main/java/cs/collaboration/yescredit/ui/existing/ExistingLoanViewModel.java
@@ -2,16 +2,121 @@ package cs.collaboration.yescredit.ui.existing;
 
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
 import javax.inject.Inject;
+
+import cs.collaboration.yescredit.model.Loan;
+import cs.collaboration.yescredit.util.Keys;
 
 public class ExistingLoanViewModel extends ViewModel {
 
     private static final String TAG = "ExistingLoanViewModel";
 
+    private final MutableLiveData<Boolean> isNotAllowed = new MutableLiveData<>();
+    private final MutableLiveData<Loan> loan = new MutableLiveData<>();
+
     @Inject
     public ExistingLoanViewModel() {
         Log.d(TAG, "ExistingLoanViewModel: view model is working...");
     }
+
+
+    public void getOnGoingLoan() {
+        loan.setValue(null);
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user != null) {
+
+            Query query = reference.child(Keys.DATABASE_NODE_LOAN)
+                    .orderByChild(Keys.DATABASE_FIELD_USER_ID)
+                    .equalTo(user.getUid());
+
+            query.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                    for (DataSnapshot singleShot : snapshot.getChildren()) {
+                        Loan loan = singleShot.getValue(Loan.class);
+                        if (loan.getStatus().equals("on-going")) {
+                            ExistingLoanViewModel.this.loan.postValue(loan);
+                        }
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
+        }
+
+    }
+
+    public void checkPendingLoan() {
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user != null) {
+
+            Query query = reference.child(Keys.DATABASE_NODE_LOAN)
+                    .orderByChild(Keys.DATABASE_FIELD_USER_ID)
+                    .equalTo(user.getUid());
+
+            query.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    long number_of_transaction = 0;
+                    for (DataSnapshot singleShot : snapshot.getChildren()) {
+                        number_of_transaction++;
+                        Loan loan = singleShot.getValue(Loan.class);
+                        Log.d(TAG, "onDataChange: done!");
+                        if (loan.getStatus().toLowerCase().equals("on-going")) {
+                            isNotAllowed.postValue(true);
+                            return;
+                        }
+
+                        if (number_of_transaction == snapshot.getChildrenCount()
+                                && !loan.getStatus().toLowerCase().equals("on-going")) {
+                            isNotAllowed.postValue(false);
+                        }
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
+        }
+    }
+
+    public LiveData<Boolean> observeIsAllowed() {
+        return isNotAllowed;
+    }
+
+
+    public LiveData<Loan> observerLoan() {
+        return loan;
+    }
+
+
 }
